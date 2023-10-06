81d093ab791f9241139146b9042810d454cb4b12
nli0601 <109606156+nli0601@users.noreply.github.com>
2023-10-06 15:30:28 +0000

    filter of connector...datetime of today...

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import xml.etree.ElementTree as ET
+import datetime
+
+today = datetime.date.today()
+todayString = today.strftime('%Y%m%d')
+
+## API
+#
+# import requests
+# from requests.auth import HTTPBasicAuth
+
+# urlUserReq='https://usclwsapdg101.US.TDWORLDWIDE.COM:1443/sap/bc/srt/rfc/sap/grac_user_existing_assgn_ws/010/grac_user_existing_assgn_ws/grac_user_existing_assgn_ws_binding'
+# headers = {'content-type': 'text/xml'}
+# body = """<?xml version="1.0" encoding="UTF-8"?>
+#          <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+
+#             <SOAP-ENV:Body><n0:GracIdmUserAsignmtServices xmlns:n0="urn:sap-com:document:sap:soap:functions:mc-style">
+#             <UserId>045292</UserId>
+#         </n0:GracIdmUserAsignmtServices></SOAP-ENV:Body>
+#          </SOAP-ENV:Envelope>"""
+
+# response = requests.post(urlUserReq,data=body,headers=headers, auth=HTTPBasicAuth('', ''))
+# print(response.content)
+
 
 systemList = []
 # Reading xml file
@@ -8,7 +31,8 @@ def xmlReading():
     rootIn = treeIn.getroot()   
     for child in rootIn.findall('item'):
         sysType = child.find('Type').text
-        if sysType == 'SYS':
+        sysName = child.find('Item').text
+        if sysType == 'SYS' and sysName.startswith('TG_') == True:
             systemName = child.find('Item').text
             systemList.append(systemName)
 
@@ -23,12 +47,11 @@ def xmlGeneration():
         ET.SubElement(item, "ItemName").text = child
         ET.SubElement(item, "Connector").text = child
         ET.SubElement(item, "ProvItemType").text = "SYS"
-        # replace with current date - datetime etc...
-        ET.SubElement(item, "ValidFrom").text = "20231006"
-        ET.SubElement(item, "ValidTo").text = "20231006"
+        ET.SubElement(item, "ValidFrom").text = todayString
+        ET.SubElement(item, "ValidTo").text = todayString
         ET.SubElement(item, "ProvAction").text = "002"
-    tree = ET.ElementTree(rootOut)
-    tree.write("filename.xml")
+    treeOut = ET.ElementTree(rootOut)
+    treeOut.write("output.xml")
 
 def main():
     xmlReading()
@@ -36,3 +59,5 @@ def main():
 
 if __name__ == "__main__":
         main()
+
+
